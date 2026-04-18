22c71ac43ae0d21e7da97a8d324a7160e6d4a931
voidhv <voidlord.public@gmail.com>
2026-04-18 15:50:56 +0200

    Jodå

--- ConsoleApp1/Game.cs
+++ ConsoleApp1/Game.cs
@@ -1,9 +1,6 @@
-﻿<<<<<<< HEAD
 ﻿global using Spectre.Console; //used globally
 using System.Diagnostics; //for debugging
-=======
-﻿global using Spectre.Console;
->>>>>>> d25ab2b008e5baed82d85789508f857481e02cd6
+
 
 Game session = new();   //creates a new game session
 
@@ -19,12 +16,8 @@ class Game
 
         while (true)
         {
-<<<<<<< HEAD
             _states.Peek().Update(); //updates the current state
-=======
-            _states.Peek().Update();
             // Debug.WriteLine($"Layers: {_states.Count}");
->>>>>>> d25ab2b008e5baed82d85789508f857481e02cd6
         }
     }
 
